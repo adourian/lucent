27156ecac89c4b2862cbd63cfccc06c86b1aa6b6
Kari Adourian <kari.adourian@gmail.com>
2025-11-24 16:29:04 +0100

    added sponsor

--- frontend/src/sponsor_to_tickr.ts
+++ frontend/src/sponsor_to_tickr.ts
@@ -989,4 +989,5 @@ export const sponsorToTicker: Record<string, string> = {
     'CSPC Ouyi Pharmaceutical Co., Ltd.': '1093.HK',
     'Shanghai Hutchison Pharmaceuticals Limited': '2607.HK',
     'Dizal Pharmaceuticals': '688192.SS',
+    'CSPC Megalith Biopharmaceutical Co.,Ltd.': '1093.HK',
 }
