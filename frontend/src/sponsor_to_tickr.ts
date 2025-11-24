415c32539205fbae6d5328ea9fdcaa7f150e3153
adourian <127963164+adourian@users.noreply.github.com>
2025-11-24 13:48:02 +0100

    Add JHM BioPharma to sponsor mapping

--- frontend/src/sponsor_to_tickr.ts
+++ frontend/src/sponsor_to_tickr.ts
@@ -985,4 +985,5 @@ export const sponsorToTicker: Record<string, string> = {
     'BioSensics': 'BIOS.BR',
     'Charles River Analytics': 'CRL',
     'Changchun GeneScience Pharmaceutical Co., Ltd.': '000661.SZ',
+    'JHM BioPharma (Tonghua) Co. , Ltd.': '600867.SS',
 }
